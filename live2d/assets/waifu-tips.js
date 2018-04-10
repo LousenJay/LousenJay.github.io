ced37db5890150d92b2dc53e6fb7d43776f1660c
LousenJay <lousenjay@gmail.com>
2018-04-10 13:36:14 +0800

    Site updated: 2018-04-10 13:36:08

--- live2d/assets/waifu-tips.js
+++ live2d/assets/waifu-tips.js
@@ -174,8 +174,8 @@ function initModel(waifuPath){
         
         /* 首次访问加载 指定模型 的 指定材质 */
         
-        var modelId = 1;            // 模型 ID
-        var modelTexturesId = 53    // 材质 ID
+        var modelId = 6;            // 模型 ID
+        var modelTexturesId = 6;    // 材质 ID
         
     } loadModel(modelId, modelTexturesId);
 	
