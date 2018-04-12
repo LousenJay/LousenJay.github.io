78fd4edeb863b8ef8a1c32b53f68b48391d37d80
LousenJay <lousenjay@gmail.com>
2018-04-12 19:41:18 +0800

    Site updated: 2018-04-12 19:41:11

--- live2d/assets/waifu-tips.js
+++ live2d/assets/waifu-tips.js
@@ -175,7 +175,7 @@ function initModel(waifuPath){
         /* 首次访问加载 指定模型 的 指定材质 */
         
         var modelId = 6;            // 模型 ID
-        var modelTexturesId = 6;    // 材质 ID
+        var modelTexturesId = 16;    // 材质 ID
         
     } loadModel(modelId, modelTexturesId);
 	
